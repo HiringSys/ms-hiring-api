f9821ae085f8a30e2eb7941de715ac4c4bdaa878
Lima152010 <lucaslbr1a2b@gmail.com>
2026-08-18 22:04:07 -0300

    build: initial Spring Boot structure

---
+++ src/test/java/com/example/hiringsys/HiringSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiringsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
